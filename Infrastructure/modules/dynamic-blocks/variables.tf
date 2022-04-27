e5de22750f770a622039e3150b89552ef80f289b
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2022-04-27 22:34:23 +0200

    made changes to variables file

--- Infrastructure/modules/dynamic-blocks/variables.tf
+++ Infrastructure/modules/dynamic-blocks/variables.tf
@@ -1,16 +1,12 @@
 variable "vpc_id"{
   type = string
-  escription = "aws vpc id"
+  description = "aws vpc id"
   default = "vpc-f8932281"
 }
 
 variable "sg_name"{
   type = string
-  escription = "aws security group name"
+  description = "aws security group name"
 }
 
-variable "vpc_id"{
-  type = string
-  escription = "aws vpc id"
-  default = "vpc-f8932281"
-}
+
